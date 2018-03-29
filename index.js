d6343eb8248acf691b309da0657be15456568b09
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-28 23:19:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -153,11 +153,12 @@ bot.on("ready", async () => {
 
 bot.on('guildCreate', guild => {
   bot.users.filter(u => u.id === '346687165868015616').forEach(user => user.send(`${guild} has just added me!`));
-	guild.users.forEach(user => {
-		if(user.hasPermission("ADMINISTRATOR"))	{
-			user.send(`🗿 **__Thank you for adding me to **${guild}**!__**🗿\n\n🌻 Start off by typing \`^help\`!\n🔘 Use \`^ad\` to advertise your server!\n💜 Do \`^twitch\` to advertise your Twitch!\n🔴 To advertise YouTube do \`^youtube\`!\n\n🗿 \`YOU HAVE ADMINISTRATOR PERMS IN A GUILD I WAS JUST ADDED TO.\` `)
-		}
-	});
+	guild.send(`🗿 **__Thank you for adding me to **${guild}**!__**🗿\n\n🌻 Start off by typing \`^help\`!\n🔘 Use \`^ad\` to advertise your server!\n💜 Do \`^twitch\` to advertise your Twitch!\n🔴 To advertise YouTube do \`^youtube\`!\n\n🗿 \`ALL SERVERS CAN HELP GROW ADBOT! ADBOT CAN HELP GROW YOU!\` `)
+//	guild.users.forEach(user => {
+	//	if(user.hasPermission("ADMINISTRATOR"))	{
+		//	user.send(`🗿 **__Thank you for adding me to **${guild}**!__**🗿\n\n🌻 Start off by typing \`^help\`!\n🔘 Use \`^ad\` to advertise your server!\n💜 Do \`^twitch\` to advertise your Twitch!\n🔴 To advertise YouTube do \`^youtube\`!\n\n🗿 \`YOU HAVE ADMINISTRATOR PERMS IN A GUILD I WAS JUST ADDED TO.\` `)
+		//}
+	//});
 });
 			 
 bot.on('guildMemberAdd', member => {
