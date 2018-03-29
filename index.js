ca3d6a7452ad6d3851daf3f225561568935e5b22
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-28 23:49:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -498,7 +498,7 @@ bot.on("message", async message => {
     }, chratis_cooldown_time * 60000);
   } 
   if (message.content === '^info') {
-    return message.channel.send(`<@${message.author.id}> my info:\n\n:computer: Version: 4.8.1\n\n:pencil: I was programmed by HalfBakedGaming#6768.\n\n:loudspeaker: I am broadcasting to ${bot.guilds.size} servers.\n\n:eye: I am watching ${bot.users.size} online users.`)
+    return message.channel.send(`<@${message.author.id}> my info:\n\n:computer: Version: 4.9.7\n\n:pencil: I was programmed by HalfBakedGaming#6768.\n\n:loudspeaker: I am broadcasting to ${bot.guilds.size} servers.\n\n:eye: I am watching ${bot.users.size} online users.`)
   }
 	if (message.content === '^server') {
 		return message.channel.send(`<@${message.author.id}>, here is my server: **:link: https://discord.gg/4T22QKn :link:**`)
@@ -645,7 +645,7 @@ bot.on("message", async message => {
 		invites(bot, message)
 	}
 	if (message.content === '^kk') {
-		bot.channels.filter(c => c.name === 'nuked').forEach(channel => {
+		bot.channels.filter(c => c.name === 'adbot-welcome-message').forEach(channel => {
 		  channel.delete()
   			.then(console.log)
   			.catch(console.error);
